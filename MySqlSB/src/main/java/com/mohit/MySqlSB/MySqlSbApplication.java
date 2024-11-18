7ea1c9cc6b6ef91ffa51fb27db186f4f3f623955
Mohit Yadav <mohityadav@Mohits-MacBook-Air.local>
2024-11-18 22:12:45 +0530

    Removing unused Jndi config

--- MySqlSB/src/main/java/com/mohit/MySqlSB/MySqlSbApplication.java
+++ MySqlSB/src/main/java/com/mohit/MySqlSB/MySqlSbApplication.java
@@ -1,7 +1,5 @@
 package com.mohit.MySqlSB;
 
-import javax.naming.Context;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,9 +10,6 @@ public class MySqlSbApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySqlSbApplication.class, args);
-		System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
-		System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
-
 	}
 	
 	@Override
